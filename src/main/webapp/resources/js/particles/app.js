ea401c608d91cb56a9f89b24c30b72211e9bab7c
Alexei Eraskin <levrun1@gmail.com>
2016-01-24 21:46:06 +0100

    move json to proper place

--- src/main/webapp/resources/js/particles/app.js
+++ src/main/webapp/resources/js/particles/app.js
@@ -1,4 +1,4 @@
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-particlesJS.load('particles-js', 'resources/particles.json', function() {
+particlesJS.load('particles-js', 'resources/js/particles/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
