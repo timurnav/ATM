ff34b42819e92c485aef3b3e9598d15fc90b7da7
Alexei Eraskin <levrun1@gmail.com>
2016-01-24 21:16:59 +0100

    fix again

--- src/main/webapp/resources/js/particles/app.js
+++ src/main/webapp/resources/js/particles/app.js
@@ -1,4 +1,4 @@
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'resources/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
