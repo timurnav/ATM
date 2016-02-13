bf8d625d82558d2beabad9fd63a984dee3571a7e
savimar <sam409@yandex.ru>
2016-02-13 18:57:02 +0300

    DAO

--- src/main/java/ru/simplewebapp/repository/AccountsRepository.java
+++ src/main/java/ru/simplewebapp/repository/AccountsRepository.java
@@ -3,9 +3,22 @@ package ru.simplewebapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.simplewebapp.model.Account;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Account, Integer> {
     Optional<Account> getByNumber(String number);
+
+    @Override
+    List<Account> findAll();
+
+    @Override
+    Account getOne(Integer integer);
+
+    @Override
+    void delete(Integer integer);
+
+    @Override
+    Account save(Account entity);
 }
 
--- src/main/java/ru/simplewebapp/repository/OperationsRepository.java
+++ src/main/java/ru/simplewebapp/repository/OperationsRepository.java
@@ -3,5 +3,18 @@ package ru.simplewebapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.simplewebapp.model.Operation;
 
+import java.util.List;
+
 public interface OperationsRepository extends JpaRepository<Operation, Integer> {
+    @Override
+    List<Operation> findAll();
+
+    @Override
+    Operation getOne(Integer integer);
+
+    @Override
+    void delete(Integer integer);
+
+    @Override
+    Operation save(Operation entity);
 }
